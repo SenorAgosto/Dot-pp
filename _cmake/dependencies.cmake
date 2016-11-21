1a7e336164ce9a2d81eb45df9a05d027f3077b2b
Austin Gilbert <ceretullis@gmail.com>
2016-11-20 20:31:47 -0600

    Checking in initial project setup and cmake configuration.

---
+++ _cmake/dependencies.cmake
@@ -0,0 +1,8 @@
+# include add our project to include path
+include_directories(${CMAKE_SOURCE_DIR})
+include(_cmake/AddLibrary/Dependencies.cmake)
+
+# our project dependencies 
+# include(_cmake/dependencies/Boost.cmake)
+include(_cmake/dependencies/UnitTest++.cmake)
+include(_cmake/dependencies/VLD.cmake)
