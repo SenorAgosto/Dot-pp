fbdab0b5100222df6dc850f91bf30007ab487237
Austin Gilbert <ceretullis@gmail.com>
2016-11-24 17:18:13 -0600

    - Adding operator<<(const TokenizerState state) for unit testing. - Adding 3 new states: LeftBracket, Equal, RightBracket

---
+++ Dot++/src/TokenizerState.cpp
@@ -0,0 +1,27 @@
+#include <Dot++/TokenizerState.hpp>
+
+namespace dot_pp {
+    
+    std::ostream& operator<<(std::ostream& os, const TokenizerState& state)
+    {
+        switch(state)
+        {
+        case TokenizerState::Init:                      os << "TokenizerState::Init"; break;
+        case TokenizerState::LeftBracket:               os << "TokenizerState::LeftBracket"; break;
+        case TokenizerState::Equal:                     os << "TokenizerState::Equal"; break;
+        case TokenizerState::RightBracket:              os << "TokenizerState::RightBracket"; break;
+        case TokenizerState::HashLineComment:           os << "TokenizerState::HashLineComment"; break;
+        case TokenizerState::BeginSlashLineComment:     os << "TokenizerState::BeginSlashLineComment"; break;
+        case TokenizerState::SlashLineComment:          os << "TokenizerState::SlashLineComment"; break;
+        case TokenizerState::MultiLineEscape:           os << "TokenizerState::MultiLineEscape"; break;
+        case TokenizerState::MultiLineComment:          os << "TokenizerState::MultiLineComment"; break;
+        case TokenizerState::EndMultiLineComment:       os << "TokenizerState::EndMultiLineComment"; break;
+        case TokenizerState::StringLiteral:             os << "TokenizerState::StringLiteral"; break;
+        case TokenizerState::StringLiteralEscape:       os << "TokenizerState::StringLiteralEscape"; break;
+        case TokenizerState::Error:                     os << "TokenizerState::Error"; break;
+        default:                                        os << "Unknown"; break;
+        };
+        
+        return os;
+    }
+}
