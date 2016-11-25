d8d91e3b8b9ff24ca99e05763898496a94ba641b
Austin Gilbert <ceretullis@gmail.com>
2016-11-24 19:13:16 -0600

    Removing unused method from Token.

--- Dot++/Token.hpp
+++ Dot++/Token.hpp
@@ -8,25 +8,18 @@ namespace dot_pp {
     {
     public:
         Token();
-
+        Token(const std::string& value, const TokenType type);
+        
         void type(const TokenType type) { type_ = type; }
         TokenType type() const { return type_; }
 
         void append(const char c) { value_.append(1, c); }
-        void clear();
-        
-        void produce() { ready_ = true; }
-        bool ready() const { return ready_; }
         
         bool empty() const { return value_.empty(); }
         const std::string& to_string() const { return value_; }
         
-        void swap(Token& t);
-        
     private:
         std::string value_;
-        
         TokenType type_;
-        bool ready_;    // set true when token is complete
     };
 }
