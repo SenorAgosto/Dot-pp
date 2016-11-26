9ee4e972353ea5643db97d7d53c969e8156699e2
Austin Gilbert <ceretullis@gmail.com>
2016-11-25 22:33:58 -0600

    Implementing MultiLineCommentState.

---
+++ Dot++/tests/unit_test/MultiLineCommentState-UT.cpp
@@ -0,0 +1,45 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <Dot++/states/MultiLineCommentState.hpp>
+#include <Dot++/TokenizerState.hpp>
+#include <Dot++/Token.hpp>
+
+#include <deque>
+
+namespace {
+    
+    struct MultiLineCommentStateFixture
+    {
+        dot_pp::Token token;
+        std::deque<dot_pp::Token> tokens;
+        dot_pp::MultiLineCommentState state;
+    };
+    
+    TEST_FIXTURE(MultiLineCommentStateFixture, verifyInstatiation)
+    {
+    }
+    
+    TEST_FIXTURE(MultiLineCommentStateFixture, verifyFunctionality)
+    {
+        CHECK_EQUAL(0U, tokens.size());
+        
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume(' ', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('a', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('/', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('#', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('[', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume(']', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('\\', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('=', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('\'', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('"', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::MultiLineComment, state.consume('\n', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::EndMultiLineComment, state.consume('*', token, tokens));
+        
+        REQUIRE CHECK_EQUAL(0U, tokens.size());
+        CHECK_EQUAL(" a/#[]\\=\'\"\n", token.to_string());
+        CHECK_EQUAL(dot_pp::TokenType::string, token.type());
+    }
+    
+}
+
