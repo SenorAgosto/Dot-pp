90b83fee3517456c26e6d5279e741ee211e0a361
Austin Gilbert <ceretullis@gmail.com>
2016-11-26 09:05:44 -0600

    Creating some helper classes for tracking information about Tokens, e.g. where they start and where they end and what file they're in.

---
+++ Dot++/TokenInfo.hpp
@@ -0,0 +1,25 @@
+#pragma once 
+#include <Dot++/LineInfo.hpp>
+#include <string>
+
+namespace dot_pp {
+
+    class TokenInfo
+    {
+    public:
+        TokenInfo(const std::string& filename);
+        TokenInfo(const std::string& filename, const LineInfo& start, const LineInfo& end);
+        
+        void start(const LineInfo& lineInfo) { start_ = lineInfo; }
+        const LineInfo& start() const { return start_; }
+        
+        void end(const LineInfo& lineInfo) { end_ = lineInfo; }
+        const LineInfo& end() const { return end_; }
+        
+    private:
+        const std::string filename_;
+        
+        LineInfo start_;
+        LineInfo end_;
+    };
+}
