325943f141e1363d7ab2fe1d355a1e7913751155
Austin Gilbert <ceretullis@gmail.com>
2016-11-25 22:33:58 -0600

    Implementing MultiLineCommentState.

---
+++ Dot++/src/states/MultiLineCommentState.cpp
@@ -0,0 +1,20 @@
+#include <Dot++/states/MultiLineCommentState.hpp>
+
+#include <Dot++/Token.hpp>
+#include <Dot++/TokenizerState.hpp>
+
+#include "./ProduceToken.hpp"
+
+namespace dot_pp {
+    
+    TokenizerState MultiLineCommentState::consume(const char c, Token& token, std::deque<Token>&) const
+    {
+        if(c == '*')
+        {
+            return TokenizerState::EndMultiLineComment;
+        }
+        
+        token.append(c);
+        return TokenizerState::MultiLineComment;
+    }
+}
