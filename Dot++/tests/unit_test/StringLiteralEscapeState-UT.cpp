a00cc53ad85a9f2c99568ca041c51fa2c4d31a4c
Austin Gilbert <ceretullis@gmail.com>
2016-11-25 21:47:46 -0600

    Implementing StringLiteralEscapeState.

---
+++ Dot++/tests/unit_test/StringLiteralEscapeState-UT.cpp
@@ -0,0 +1,33 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <Dot++/states/StringLiteralEscapeState.hpp>
+#include <Dot++/TokenizerState.hpp>
+#include <Dot++/Token.hpp>
+
+#include <deque>
+
+namespace {
+    
+    struct StringLiteralEscapeStateFixture
+    {
+        dot_pp::Token token;
+        std::deque<dot_pp::Token> tokens;
+        dot_pp::StringLiteralEscapeState state;
+    };
+    
+    TEST_FIXTURE(StringLiteralEscapeStateFixture, verifyInstatiation)
+    {
+    }
+    
+    TEST_FIXTURE(StringLiteralEscapeStateFixture, verifyWeAlwaysStayInErrorState)
+    {
+        CHECK_EQUAL(0U, tokens.size());
+        
+        CHECK_EQUAL(dot_pp::TokenizerState::StringLiteral, state.consume(' ', token, tokens));
+        CHECK_EQUAL(dot_pp::TokenizerState::StringLiteral, state.consume('"', token, tokens));
+        
+        CHECK_EQUAL(0U, tokens.size());
+        CHECK_EQUAL(" \"", token.to_string());
+    }
+}
+
