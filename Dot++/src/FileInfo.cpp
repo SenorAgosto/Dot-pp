bb5df530f54b9aca29f551f09716a99f49bf1027
Austin Gilbert <ceretullis@gmail.com>
2016-11-28 18:00:35 -0600

    Adding unit test coverage for FileInfo.

--- Dot++/src/FileInfo.cpp
+++ Dot++/src/FileInfo.cpp
@@ -1,4 +1,6 @@
 #include <Dot++/FileInfo.hpp>
+#include <Dot++/Token.hpp>
+
 #include <utility>
 
 namespace dot_pp {
@@ -11,6 +13,7 @@ namespace dot_pp {
     FileInfo::FileInfo(const std::string& filename, const LineInfo& start)
         : filename_(filename)
         , start_(start)
+        , end_(start)
     {
     }
     
@@ -33,5 +36,20 @@ namespace dot_pp {
         , start_(info.start_)
         , end_(std::move(info.end_))
     {
-    }    
+    }
+    
+    void FileInfo::advanceBy(const Token& token)
+    {
+        for(const auto c : token.to_string())
+        {
+            if(c == '\n')
+            {
+                end_.incrementLine();
+            }
+            else
+            {
+                end_.incrementColumn();
+            }
+        }
+    }
 }
