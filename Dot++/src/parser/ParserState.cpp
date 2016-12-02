6a0856fa00e19a2a22f368b8de51a1c127079c79
Austin Gilbert <ceretullis@gmail.com>
2016-12-01 23:24:20 -0600

    Renaming a state in ParserState.

--- Dot++/src/parser/ParserState.cpp
+++ Dot++/src/parser/ParserState.cpp
@@ -15,7 +15,7 @@ namespace dot_pp { namespace parser {
         case ParserState::ReadEdgeToken:            return os << "ParserState::ReadEdgeToken";
         case ParserState::CreateEdge:               return os << "ParserState::CreateEdge";
         case ParserState::ReadGraphAttributeEqual:  return os << "ParserState::ReadGraphAttributeEqual";
-        case ParserState::ReadAttributeValue:       return os << "ParserState::ReadAttributeValue";
+        case ParserState::ReadGraphAttributeValue:  return os << "ParserState::ReadGraphAttributeValue";
         case ParserState::ReadRightBracket:         return os << "ParserState::ReadRightBracket";
         case ParserState::ReadAttributeName:        return os << "ParserState::ReadAttributeName";
         case ParserState::ReadAttributeEqual:       return os << "ParserState::ReadAttributeEqual";
