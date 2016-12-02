5cf5762665bf1ebd2edcde9df69beddb3b5a00ba
Austin Gilbert <ceretullis@gmail.com>
2016-12-01 23:26:50 -0600

    Implementing parser::InitialState

---
+++ Dot++/tests/unit_test/Parser-InitState-UT.cpp
@@ -0,0 +1,64 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/lexer/TokenInfo.hpp>
+#include <Dot++/parser/states/InitialState.hpp>
+
+namespace {
+    
+    using namespace dot_pp::lexer;
+    using namespace dot_pp::parser;
+    
+    struct InitialStateFixture
+    {
+        TokenStack stack;
+        states::InitialState state;
+    };
+    
+    TEST_FIXTURE(InitialStateFixture, verifyInstatiation)
+    {
+    }
+    
+    TEST_FIXTURE(InitialStateFixture, verifyMovesToGraphKeywordOnKeywordTokenAndIgnoresComments)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("digraph", TokenType::keyword), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah blah", TokenType::comment), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah blah", TokenType::multiline_comment), FileInfo("test.dot"));
+
+        auto handle = tokens.cbegin();
+        
+        CHECK_EQUAL(ParserState::GraphKeyword, state.consume(handle++, stack));
+        CHECK_EQUAL(ParserState::Init, state.consume(handle++, stack));
+        CHECK_EQUAL(ParserState::Init, state.consume(handle++, stack));
+    }
+    
+    TEST_FIXTURE(InitialStateFixture, verifyThrowsOnWrongTokenType)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("string", TokenType::string), FileInfo("test.dot"));
+        tokens.emplace_back(Token("string_lit", TokenType::string_literal), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_paren", TokenType::l_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_paren", TokenType::r_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("edge", TokenType::edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("edge", TokenType::directed_edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_bracket", TokenType::l_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_bracket", TokenType::r_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token("equal", TokenType::equal), FileInfo("test.dot"));
+        tokens.emplace_back(Token("end_statement", TokenType::end_statement), FileInfo("test.dot"));
+        
+        for(auto handle = tokens.cbegin(), end = tokens.cend(); handle != end; ++handle)
+        {
+            CHECK_THROW(state.consume(handle, stack), dot_pp::SyntaxError);
+        }
+    }
+    
+    TEST_FIXTURE(InitialStateFixture, verifyThrowsOnUnknownKeyword)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("keyword", TokenType::keyword), FileInfo("test.dot"));
+        
+        auto handle = tokens.begin();
+        CHECK_THROW(state.consume(handle, stack), dot_pp::SyntaxError);
+    }
+}
