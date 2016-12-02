9fc966932276df3f54d9c1f1441d9dc98ee66c50
Austin Gilbert <ceretullis@gmail.com>
2016-12-01 22:13:28 -0600

    parser: defining parser states

---
+++ Dot++/parser/ParserState.hpp
@@ -0,0 +1,29 @@
+#pragma once 
+#include <ostream>
+
+namespace dot_pp { namespace parser {
+    
+    enum class ParserState
+    {
+        Init,
+        
+        GraphKeyword,               // digraph | graph
+        GraphName,                  // string graph name
+        BeginGraph,                 // {
+        
+        IgnoreSubgraphs,            // any { after the initial graph begins until } ends graph
+        
+        ReadStringToken,            // push string token
+        ReadEdgeToken,              // edge | directed_edge, pop string token, create vertex, push string token
+        CreateEdge,                 // create vertex, pop token, create edge, push edge vertex1, push edge vertex2
+        
+        ReadGraphAttributeEqual,    // =
+        ReadAttributeValue,         // string | string_literal token, pop token, create graph attribute
+        
+        ReadRightBracket,           // [
+        ReadAttributeName,          // push string token
+        ReadAttributeEqual,         // pop token, create adge attribute
+    };
+    
+    std::ostream& operator<<(std::ostream& os, const ParserState state);
+}}
