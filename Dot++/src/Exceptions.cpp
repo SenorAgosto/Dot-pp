6be0fac361cd4ea0554fffb85b9e98eeafb2eab9
Austin Gilbert <ceretullis@gmail.com>
2016-12-01 22:29:00 -0600

    Defining SyntaxError exception.

--- Dot++/src/Exceptions.cpp
+++ Dot++/src/Exceptions.cpp
@@ -1,4 +1,6 @@
 #include <Dot++/Exceptions.hpp>
+#include <Dot++/lexer/TokenInfo.hpp>
+
 #include <sstream>
 
 namespace dot_pp {
@@ -12,6 +14,19 @@ namespace dot_pp {
             
             return ss.str();
         }
+        
+        std::string constructFileLocationString(const lexer::TokenInfo& info)
+        {
+            std::stringstream ss;
+            ss << info.fileInfo().end().line() << ":" << info.fileInfo().end().column();
+            
+            return ss.str();
+        }
+    }
+
+    ParserError::ParserError(const std::string& reason)
+        : std::runtime_error(reason)
+    {
     }
     
     TokenizerError::TokenizerError(const std::string& reason)
@@ -21,6 +36,11 @@ namespace dot_pp {
     
     UnknownTokenizerState::UnknownTokenizerState(const lexer::TokenizerState state)
         : TokenizerError(constructError("Unknown Tokenizer State: ", state))
+    {
+    }
+    
+    SyntaxError::SyntaxError(const std::string& error, const lexer::TokenInfo& info)
+        : ParserError("Syntax Error: " + constructFileLocationString(info) + ": " + error)
     {
     }
 }
