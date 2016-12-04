952b986a64ad2e67381fc52e5b642c5fd50ed22d
Austin Gilbert <ceretullis@gmail.com>
2016-12-04 14:06:51 -0600

    Defining TokenStack in terms of TokenInfoHandle.

--- Dot++/parser/TokenStack.hpp
+++ Dot++/parser/TokenStack.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <Dot++/lexer/TokenInfo.hpp>
+#include <Dot++/parser/TokenInfoHandle.hpp>
 #include <deque>
 #include <stack>
 
 namespace dot_pp { namespace parser {
 
-    using TokenStack = std::stack<std::deque<lexer::TokenInfo>::const_iterator>;
+    using TokenStack = std::stack<TokenInfoHandle>;
 }}
