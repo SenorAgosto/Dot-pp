332e0dcb150e533d86607f1f2658093286d794d7
Austin Gilbert <ceretullis@gmail.com>
2016-12-04 17:21:36 -0600

    parser: implementing parser::states::ReadEdgeTokenState

---
+++ Dot++/parser/states/ReadEdgeTokenState.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <Dot++/parser/ParserStateInterface.hpp>
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/lexer/Token.hpp>
+#include <Dot++/lexer/TokenType.hpp>
+
+namespace dot_pp { namespace parser { namespace states {
+
+    template<class ConstructionPolicy>
+    class ReadEdgeTokenState : public ParserStateInterface<ConstructionPolicy>
+    {
+    public:
+        ParserState consume(const TokenInfoHandle& handle, TokenStack& stack, ConstructionPolicy& constructor) override
+        {
+            const auto& token = handle->token();
+
+            if((token.type() == lexer::TokenType::comment) || (token.type() == lexer::TokenType::multiline_comment))
+            {
+                return ParserState::ReadEdgeToken;
+            }
+            
+            if(token.type() == lexer::TokenType::string)
+            {
+                const auto vertex1 = stack.top();
+                const auto vertex1Token = vertex1->token();
+                
+                const auto vertex2 = token.to_string();
+                constructor.createVertex(vertex2);
+                constructor.createEdge(vertex1Token.to_string(), vertex2);
+                
+                // pop top, push handle
+                stack.top() = handle;
+                return ParserState::CreatedEdge;
+            }
+        
+            throw dot_pp::SyntaxError("Unexpected token encountered, expected 'string', found '" + token.to_string() + "'", *handle);
+            
+
+        }
+    };
+}}}
---
+++ Dot++/tests/unit_test/Parser-ReadEdgeTokenState-UT.cpp
@@ -0,0 +1,96 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/lexer/TokenInfo.hpp>
+#include <Dot++/parser/states/ReadEdgeTokenState.hpp>
+
+#include <string> 
+#include <utility>
+
+namespace {
+    
+    using namespace dot_pp::lexer;
+    using namespace dot_pp::parser;
+
+    struct NullConstructionPolicy
+    {
+        void createGraph(const std::string&) {}
+        void createDigraph(const std::string&) {}
+        
+        void createVertex(const std::string& name) { vertex = name; }
+        void createEdge(const std::string& v1, const std::string& v2){ edge = std::make_pair(v1, v2); }
+        
+        void applyGraphAttribute(const std::string&, const std::string&){}
+        void applyVertexAttribute(const std::string&, const std::string&, const std::string&) {}
+        
+        void applyEdgeAttribute(const std::string&, const std::string&, const std::string&, const std::string&) {}
+        void finalize() {}
+        
+        std::pair<std::string, std::string> edge;
+        std::string vertex;
+    };
+    
+    struct ReadEdgeTokenStateFixture
+    {
+        TokenStack stack;
+        NullConstructionPolicy constructor;
+        
+        states::ReadEdgeTokenState<NullConstructionPolicy> state;
+    };
+    
+    TEST_FIXTURE(ReadEdgeTokenStateFixture, verifyInstatiation)
+    {
+    }
+    
+    TEST_FIXTURE(ReadEdgeTokenStateFixture, verifyTransitionsToCreatedEdgeTokenOnEdge)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("a", TokenType::string), FileInfo("test.dot"));
+        tokens.emplace_back(Token("b", TokenType::string), FileInfo("test.dot"));
+        
+        auto handle = tokens.cbegin();
+        stack.push(handle++);
+        
+        CHECK_EQUAL(1U, stack.size());
+        CHECK_EQUAL(ParserState::CreatedEdge, state.consume(handle++, stack, constructor));
+        CHECK_EQUAL(1U, stack.size());
+        
+        CHECK_EQUAL("b", constructor.vertex);
+        CHECK_EQUAL("a", constructor.edge.first);
+        CHECK_EQUAL("b", constructor.edge.second);
+    }
+    
+    TEST_FIXTURE(ReadEdgeTokenStateFixture, verifyCommentsAreIgnored)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("blah blah", TokenType::comment), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah \n blah", TokenType::multiline_comment), FileInfo("test.dot"));
+
+        auto handle = tokens.cbegin();
+        
+        CHECK_EQUAL(ParserState::ReadEdgeToken, state.consume(handle++, stack, constructor));
+        CHECK_EQUAL(0U, stack.size());
+        
+        CHECK_EQUAL(ParserState::ReadEdgeToken, state.consume(handle++, stack, constructor));
+        CHECK_EQUAL(0U, stack.size());
+    }
+    
+    TEST_FIXTURE(ReadEdgeTokenStateFixture, verifyThrowsOnInvalidTokens)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("string_lit", TokenType::string_literal), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_paren", TokenType::l_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_paren", TokenType::r_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("edge", TokenType::edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("edge", TokenType::directed_edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_bracket", TokenType::l_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_bracket", TokenType::r_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token("equal", TokenType::equal), FileInfo("test.dot"));
+        tokens.emplace_back(Token("end_statement", TokenType::end_statement), FileInfo("test.dot"));
+        
+        for(auto handle = tokens.cbegin(), end = tokens.cend(); handle != end; ++handle)
+        {
+            CHECK_THROW(state.consume(handle, stack, constructor), dot_pp::SyntaxError);
+        }
+    }
+}
