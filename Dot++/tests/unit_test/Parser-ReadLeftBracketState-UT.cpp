1bbe3738960b8ed586c565d34e24ac36a6ea7f3e
Austin Gilbert <ceretullis@gmail.com>
2016-12-04 19:04:02 -0600

    parser: implementing parser::states::ReadLeftBracket

---
+++ Dot++/tests/unit_test/Parser-ReadLeftBracketState-UT.cpp
@@ -0,0 +1,72 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/NullConstructionPolicy.hpp>
+
+#include <Dot++/lexer/TokenInfo.hpp>
+#include <Dot++/parser/states/ReadLeftBracketState.hpp>
+
+#include <string>
+#include <utility>
+
+namespace {
+    
+    using namespace dot_pp::lexer;
+    using namespace dot_pp::parser;
+    
+    struct ReadLeftBracketStateFixture
+    {
+        TokenStack stack;
+        dot_pp::NullConstructionPolicy constructor;
+        
+        states::ReadLeftBracketState<dot_pp::NullConstructionPolicy> state;
+    };
+    
+    TEST_FIXTURE(ReadLeftBracketStateFixture, verifyInstatiation)
+    {
+    }
+    
+    TEST_FIXTURE(ReadLeftBracketStateFixture, verifyTransitionsToEdgeAttributeReadOnString)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("attribute", TokenType::string), FileInfo("test.dot"));
+        
+        auto handle = tokens.cbegin();
+        
+        CHECK_EQUAL(0U, stack.size());
+        CHECK_EQUAL(ParserState::ReadAttributeName, state.consume(handle++, stack, constructor));
+        CHECK_EQUAL(1U, stack.size());
+    }
+
+    TEST_FIXTURE(ReadLeftBracketStateFixture, verifyTransitionsToGraphAttributeValueRightBracket)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("]", TokenType::r_bracket), FileInfo("test.dot"));
+        
+        auto handle = tokens.cbegin();
+        
+        CHECK_EQUAL(0U, stack.size());
+        CHECK_EQUAL(ParserState::ReadGraphAttributeValue, state.consume(handle++, stack, constructor));
+        CHECK_EQUAL(0U, stack.size());
+    }
+    
+    TEST_FIXTURE(ReadLeftBracketStateFixture, verifyThrowsOnInvalidTokens)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("keyword", TokenType::keyword), FileInfo("test.dot"));
+        tokens.emplace_back(Token("string_lit", TokenType::string_literal), FileInfo("test.dot"));
+        tokens.emplace_back(Token("edge", TokenType::edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("directed_edge", TokenType::directed_edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_paren", TokenType::l_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_paren", TokenType::r_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_bracket", TokenType::l_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token(";", TokenType::end_statement), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah blah", TokenType::comment), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah \n blah", TokenType::multiline_comment), FileInfo("test.dot"));
+        
+        for(auto handle = tokens.cbegin(), end = tokens.cend(); handle != end; ++handle)
+        {
+            CHECK_THROW(state.consume(handle, stack, constructor), dot_pp::SyntaxError);
+        }
+    }
+}
