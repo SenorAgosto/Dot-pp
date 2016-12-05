642069e3835dfde7d1579c229f21847de9a797f1
Austin Gilbert <ceretullis@gmail.com>
2016-12-04 21:19:13 -0600

    Initial implementation of dot_pp::Parser and dot_pp::parser::Parser.

--- Dot++/Parser.hpp
+++ Dot++/Parser.hpp
@@ -1,49 +1,44 @@
 #pragma once 
 #include <Dot++/lexer/Tokenizer.hpp>
 #include <Dot++/lexer/TokenInfo.hpp>
+#include <Dot++/parser/Parser.hpp>
 
 #include <deque>
 #include <istream>
 
 namespace dot_pp {
     
-    template<class ConstructionPolicy, class LoggingInterface>
+    template<class ConstructionPolicy>
     class Parser
     {
     public:
-        Parser(LoggingInterface& logHandle);
+        Parser(ConstructionPolicy& constructor);
         void parse(std::istream& stream, const std::string& filename, ConstructionPolicy& policy);
     
     private:
         std::deque<lexer::TokenInfo> tokenize(std::istream& stream, const std::string& filename);
+        void parseTokens(const std::deque<lexer::TokenInfo>& tokens);
         
     private:
-        LoggingInterface& log_;
+        ConstructionPolicy& constructor_;
     };
     
     
-    template<class ConstructionPolicy, class LoggingInterface>
-    Parser<ConstructionPolicy, LoggingInterface>::Parser(LoggingInterface& logHandle)
-        : log_(logHandle)
+    template<class ConstructionPolicy>
+    Parser<ConstructionPolicy>::Parser(ConstructionPolicy& constructor)
+        : constructor_(constructor)
     {
     }
     
-    template<class ConstructionPolicy, class LoggingInterface>
-    void Parser<ConstructionPolicy, LoggingInterface>::parse(std::istream& stream, const std::string& filename, ConstructionPolicy& /*policy*/)
+    template<class ConstructionPolicy>
+    void Parser<ConstructionPolicy>::parse(std::istream& stream, const std::string& filename, ConstructionPolicy& /*policy*/)
     {
-        try
-        {
-            auto tokens = tokenize(stream, filename);
-            // TODO: iterate over tokens constructing AST
-        }
-        catch(const std::exception& e)
-        {
-            log_.error(std::string("Exception: ") + e.what());
-        }
+        auto tokens = tokenize(stream, filename);
+        parseTokens(tokens);
     }
     
-    template<class ConstructionPolicy, class LoggingInterface>
-    std::deque<lexer::TokenInfo> Parser<ConstructionPolicy, LoggingInterface>::tokenize(std::istream& stream, const std::string& filename)
+    template<class ConstructionPolicy>
+    std::deque<lexer::TokenInfo> Parser<ConstructionPolicy>::tokenize(std::istream& stream, const std::string& filename)
     {
         lexer::Tokenizer tokenizer(filename);
         std::deque<lexer::TokenInfo> tokens;
@@ -55,5 +50,22 @@ namespace dot_pp {
         }
         
         return tokens;
+    }
+    
+    template<class ConstructionPolicy>
+    void Parser<ConstructionPolicy>::parseTokens(const std::deque<lexer::TokenInfo>& tokens)
+    {
+        parser::Parser<ConstructionPolicy> parser(constructor_);
+        
+        for(auto handle = tokens.cbegin(), end = tokens.cend(); handle != end; ++handle)
+        {
+            const auto& token = handle->token();
+            if((token.type() == lexer::TokenType::comment) || (token.type() == lexer::TokenType::multiline_comment))
+            {
+                continue;
+            }
+            
+            parser.consume(handle);
+        }
     }
 }
