708a6a803e582cca53082b6c4033f4ac6e7372f7
Austin Gilbert <ceretullis@gmail.com>
2016-12-04 19:13:07 -0600

    parser: implementing parser::states::ReadAttributeNameState

---
+++ Dot++/parser/states/ReadAttributeNameState.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <Dot++/parser/ParserStateInterface.hpp>
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/lexer/Token.hpp>
+#include <Dot++/lexer/TokenType.hpp>
+
+namespace dot_pp { namespace parser { namespace states {
+
+    template<class ConstructionPolicy>
+    class ReadAttributeNameState : public ParserStateInterface<ConstructionPolicy>
+    {
+    public:
+        ParserState consume(const TokenInfoHandle& handle, TokenStack&, ConstructionPolicy&) override
+        {
+            const auto& token = handle->token();
+
+            if(token.type() == lexer::TokenType::equal)
+            {
+                return ParserState::ReadAttributeEqual;
+            }
+            
+            throw dot_pp::SyntaxError("Unexpected token encountered, expected '=', found '" + token.to_string() + "'", *handle);
+        }
+    };
+}}}
---
+++ Dot++/tests/unit_test/Parser-ReadAttributeNameState-UT.cpp
@@ -0,0 +1,62 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/NullConstructionPolicy.hpp>
+
+#include <Dot++/lexer/TokenInfo.hpp>
+#include <Dot++/parser/states/ReadAttributeNameState.hpp>
+
+#include <string>
+#include <utility>
+
+namespace {
+    
+    using namespace dot_pp::lexer;
+    using namespace dot_pp::parser;
+    
+    struct ReadAttributeNameStateFixture
+    {
+        TokenStack stack;
+        dot_pp::NullConstructionPolicy constructor;
+        
+        states::ReadAttributeNameState<dot_pp::NullConstructionPolicy> state;
+    };
+    
+    TEST_FIXTURE(ReadAttributeNameStateFixture, verifyInstatiation)
+    {
+    }
+    
+    TEST_FIXTURE(ReadAttributeNameStateFixture, verifyTransitionsToEdgeAttributeReadOnString)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("=", TokenType::equal), FileInfo("test.dot"));
+        
+        auto handle = tokens.cbegin();
+        
+        CHECK_EQUAL(0U, stack.size());
+        CHECK_EQUAL(ParserState::ReadAttributeEqual, state.consume(handle++, stack, constructor));
+        CHECK_EQUAL(0U, stack.size());
+    }
+    
+    TEST_FIXTURE(ReadAttributeNameStateFixture, verifyThrowsOnInvalidTokens)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("keyword", TokenType::keyword), FileInfo("test.dot"));
+        tokens.emplace_back(Token("string", TokenType::string), FileInfo("test.dot"));
+        tokens.emplace_back(Token("string_lit", TokenType::string_literal), FileInfo("test.dot"));
+        tokens.emplace_back(Token("edge", TokenType::edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("directed_edge", TokenType::directed_edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_paren", TokenType::l_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_paren", TokenType::r_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_bracket", TokenType::l_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_bracket", TokenType::r_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token(";", TokenType::end_statement), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah blah", TokenType::comment), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah \n blah", TokenType::multiline_comment), FileInfo("test.dot"));
+        
+        for(auto handle = tokens.cbegin(), end = tokens.cend(); handle != end; ++handle)
+        {
+            CHECK_THROW(state.consume(handle, stack, constructor), dot_pp::SyntaxError);
+        }
+    }
+}
