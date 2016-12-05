778f7d91795c7d5cc369ff0c6b3a54ddaab9430f
Austin Gilbert <ceretullis@gmail.com>
2016-12-04 19:34:50 -0600

    Changing behavior of parser::states::CreatedEdgeState and parser::states::ReadGraphAttributeValueState to clear the stack before changing state.

--- Dot++/parser/states/ReadGraphAttributeValueState.hpp
+++ Dot++/parser/states/ReadGraphAttributeValueState.hpp
@@ -11,12 +11,13 @@ namespace dot_pp { namespace parser { namespace states {
     class ReadGraphAttributeValueState : public ParserStateInterface<ConstructionPolicy>
     {
     public:
-        ParserState consume(const TokenInfoHandle& handle, TokenStack&, ConstructionPolicy&) override
+        ParserState consume(const TokenInfoHandle& handle, TokenStack& stack, ConstructionPolicy&) override
         {
             const auto& token = handle->token();
             
             if(token.type() == lexer::TokenType::end_statement)
             {
+                clear(stack);
                 return ParserState::BeginGraph;
             }
             
