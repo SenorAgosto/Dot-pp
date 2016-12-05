97b4f251bcce0f36a06dbc5af3840b3ce5799ddf
Austin Gilbert <ceretullis@gmail.com>
2016-12-04 18:46:36 -0600

    parser: implementing parser::states::ReadGraphAttributeEqualState

---
+++ Dot++/parser/states/ReadGraphAttributeEqualState.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <Dot++/parser/ParserStateInterface.hpp>
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/lexer/Token.hpp>
+#include <Dot++/lexer/TokenType.hpp>
+
+namespace dot_pp { namespace parser { namespace states {
+
+    template<class ConstructionPolicy>
+    class ReadGraphAttributeEqualState : public ParserStateInterface<ConstructionPolicy>
+    {
+    public:
+        ParserState consume(const TokenInfoHandle& handle, TokenStack& stack, ConstructionPolicy& constructor) override
+        {
+            const auto& token = handle->token();
+        
+            if((token.type() == lexer::TokenType::string) || (token.type() == lexer::TokenType::string_literal))
+            {
+                const auto attribute = stack.top();
+                const auto attributeToken = attribute->token();
+                stack.pop();
+                
+                constructor.applyGraphAttribute(attributeToken.to_string(), token.to_string());
+                
+                return ParserState::ReadGraphAttributeValue;
+            }
+            
+            throw dot_pp::SyntaxError("Unexpected token encountered, expected 'string' or 'string_literal', found '" + token.to_string() + "'", *handle);
+        }
+    };
+}}}
---
+++ Dot++/tests/unit_test/Parser-ReadGraphAttributeEqualState-UT.cpp
@@ -0,0 +1,101 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/lexer/TokenInfo.hpp>
+#include <Dot++/parser/states/ReadGraphAttributeEqualState.hpp>
+
+#include <string>
+#include <utility>
+
+namespace {
+    
+    using namespace dot_pp::lexer;
+    using namespace dot_pp::parser;
+
+    struct NullConstructionPolicy
+    {
+        NullConstructionPolicy()
+        {
+        }
+        
+        void createGraph(const std::string&) {}
+        void createDigraph(const std::string&) {}
+        
+        void createVertex(const std::string&) {}
+        void createEdge(const std::string&, const std::string&){}
+        
+        void applyGraphAttribute(const std::string& attr, const std::string& value) { attribute = std::make_pair(attr, value); }
+        void applyVertexAttribute(const std::string&, const std::string&, const std::string&) {}
+        
+        void applyEdgeAttribute(const std::string&, const std::string&, const std::string&, const std::string&) {}
+        void finalize() {}
+        
+        std::pair<std::string, std::string> attribute;
+    };
+    
+    struct ReadGraphAttributeEqualStateFixture
+    {
+        TokenStack stack;
+        NullConstructionPolicy constructor;
+        
+        states::ReadGraphAttributeEqualState<NullConstructionPolicy> state;
+    };
+    
+    TEST_FIXTURE(ReadGraphAttributeEqualStateFixture, verifyInstatiation)
+    {
+    }
+    
+    TEST_FIXTURE(ReadGraphAttributeEqualStateFixture, verifyTransitionsToGraphAttributeValueOnString)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("attributeName", TokenType::string), FileInfo("test.dot"));
+        tokens.emplace_back(Token("attributeValue", TokenType::string), FileInfo("test.dot"));
+        
+        auto handle = tokens.cbegin();
+        stack.push(handle++);
+        
+        CHECK_EQUAL(1U, stack.size());
+        CHECK_EQUAL(ParserState::ReadGraphAttributeValue, state.consume(handle++, stack, constructor));
+        
+        CHECK_EQUAL(0U, stack.size());
+        CHECK_EQUAL("attributeName", constructor.attribute.first);
+        CHECK_EQUAL("attributeValue", constructor.attribute.second);
+    }
+
+    TEST_FIXTURE(ReadGraphAttributeEqualStateFixture, verifyTransitionsToGraphAttributeValueOnStringLiteral)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("attributeName", TokenType::string), FileInfo("test.dot"));
+        tokens.emplace_back(Token("attributeValue", TokenType::string_literal), FileInfo("test.dot"));
+        
+        auto handle = tokens.cbegin();
+        stack.push(handle++);
+        
+        CHECK_EQUAL(1U, stack.size());
+        CHECK_EQUAL(ParserState::ReadGraphAttributeValue, state.consume(handle++, stack, constructor));
+
+        CHECK_EQUAL(0U, stack.size());
+        CHECK_EQUAL("attributeName", constructor.attribute.first);
+        CHECK_EQUAL("attributeValue", constructor.attribute.second);
+    }
+    
+    TEST_FIXTURE(ReadGraphAttributeEqualStateFixture, verifyThrowsOnInvalidTokens)
+    {
+        std::deque<TokenInfo> tokens;
+        tokens.emplace_back(Token("keyword", TokenType::keyword), FileInfo("test.dot"));
+        tokens.emplace_back(Token("edge", TokenType::edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("directed_edge", TokenType::directed_edge), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_paren", TokenType::l_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_paren", TokenType::r_paren), FileInfo("test.dot"));
+        tokens.emplace_back(Token("l_bracket", TokenType::l_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token("r_bracket", TokenType::r_bracket), FileInfo("test.dot"));
+        tokens.emplace_back(Token("end_statement", TokenType::end_statement), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah blah", TokenType::comment), FileInfo("test.dot"));
+        tokens.emplace_back(Token("blah \n blah", TokenType::multiline_comment), FileInfo("test.dot"));
+        
+        for(auto handle = tokens.cbegin(), end = tokens.cend(); handle != end; ++handle)
+        {
+            CHECK_THROW(state.consume(handle, stack, constructor), dot_pp::SyntaxError);
+        }
+    }
+}
