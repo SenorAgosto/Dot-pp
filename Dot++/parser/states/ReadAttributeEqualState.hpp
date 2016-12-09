2f85b1f99b7323eb75f6ba67eca03518fb93a250
Austin Gilbert <ceretullis@gmail.com>
2016-12-08 21:01:27 -0600

    parser: updating logic to apply edge attributes to every edge in a chain.

--- Dot++/parser/states/ReadAttributeEqualState.hpp
+++ Dot++/parser/states/ReadAttributeEqualState.hpp
@@ -17,6 +17,40 @@ namespace dot_pp { namespace parser { namespace states {
             
             if((token.type() == lexer::TokenType::string) || (token.type() == lexer::TokenType::string_literal))
             {
+                TokenStack temp;
+
+                const auto attribute = attributes.top();
+                const auto attributeToken = attribute->token();
+                attributes.pop();
+                
+                auto v2 = stack.top();
+                auto v2Token = v2->token();
+                
+                temp.push(v2);
+                stack.pop();
+                
+                do
+                {
+                    const auto v1 = stack.top();
+                    const auto v1Token = v1->token();
+                    
+                    constructor.applyEdgeAttribute(v1Token.to_string(), v2Token.to_string(), attributeToken.to_string(), token.to_string());
+                    
+                    v2 = v1;
+                    v2Token = v1Token;
+
+                    temp.push(stack.top());
+                    stack.pop();
+                    
+                } while(!stack.empty());
+                
+                while(!temp.empty())
+                {
+                    stack.push(temp.top());
+                    temp.pop();
+                }
+                
+                /*
                 const auto attribute = attributes.top();
                 const auto attributeToken = attribute->token();
                 attributes.pop();
@@ -31,7 +65,7 @@ namespace dot_pp { namespace parser { namespace states {
                 stack.push(v2);
                 
                 constructor.applyEdgeAttribute(v1Token.to_string(), v2Token.to_string(), attributeToken.to_string(), token.to_string());
-                
+                */
                 return ParserState::ReadLeftBracket;
             }
             
