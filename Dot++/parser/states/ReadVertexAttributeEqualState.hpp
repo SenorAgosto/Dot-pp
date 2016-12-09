3b6237e8c95a33e83639e28bc35d498e56e7701b
Austin Gilbert <ceretullis@gmail.com>
2016-12-08 18:45:54 -0600

    parser: implementing parser::states::ReadVertexAttributeEqualState.

---
+++ Dot++/parser/states/ReadVertexAttributeEqualState.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <Dot++/parser/ParserStateInterface.hpp>
+
+#include <Dot++/Exceptions.hpp>
+#include <Dot++/lexer/Token.hpp>
+#include <Dot++/lexer/TokenType.hpp>
+
+namespace dot_pp { namespace parser { namespace states {
+
+    template<class ConstructionPolicy>
+    class ReadVertexAttributeEqualState : public ParserStateInterface<ConstructionPolicy>
+    {
+    public:
+        ParserState consume(const TokenInfoHandle& handle, TokenStack& stack, ConstructionPolicy& constructor) override
+        {
+            const auto& token = handle->token();
+        
+            if((token.type() == lexer::TokenType::string) || (token.type() == lexer::TokenType::string_literal))
+            {
+                const auto attribute = stack.top();
+                const auto attributeToken = attribute->token();
+                stack.pop();
+                
+                const auto vertex = stack.top();
+                const auto vertexToken = vertex->token();
+                
+                constructor.applyVertexAttribute(vertexToken.to_string(), attributeToken.to_string(), token.to_string());
+                return ParserState::ReadLeftBracketVertexAttribute;
+            }
+            
+            throw dot_pp::SyntaxError("Unexpected token found, expected '<string>' or '<string_literal>', found '" + token.to_string() + "'", *handle);
+        }
+    };
+}}}
