ccf1636424f5116e9319a2ab0b7cf21b97685343
Austin Gilbert <ceretullis@gmail.com>
2016-12-29 22:58:29 -0600

    Implementing SerializationPolicy::applyVertexAttributes()

--- Dot++/SerializationPolicy.hpp
+++ Dot++/SerializationPolicy.hpp
@@ -1,12 +1,17 @@
 #pragma once 
 #include <ostream>
 #include <string>
+#include <utility>
 
 namespace dot_pp {
 
     class SerializationPolicy
     {
     public:
+        using AttributeName = std::string;
+        using AttributeValue = std::string;
+        using Attribute = std::pair<AttributeName, AttributeValue>;
+    
         SerializationPolicy(std::ostream& stream);
         
         void createGraph(const std::string& name = "");
@@ -19,8 +24,32 @@ namespace dot_pp {
         void createEdge(const std::string& vertex1, const std::string& vertex2, const std::string& attributeName, const std::string& value);
         
         void applyGraphAttribute(const std::string& attributeName, const std::string& value);
-
+        
+        template<typename... AttributeList>
+        void applyVertexAttributes(const std::string& vertex, AttributeList&&... attributes)
+        {
+            stream_ << "\t" << vertex << " [";
+            printAttributes(std::forward<AttributeList>(attributes)...);
+            stream_ << "];\n";
+        }
+        
         void finalize();
+        
+    private:
+        void printAttribute(const Attribute& attribute);
+        
+        template<typename T>
+        void printAttributes(const T& attribute)
+        {
+            printAttribute(attribute);
+        }
+        
+        template<typename T, typename... AttributeList>
+        void printAttributes(const T& attribute, AttributeList&&... attributes)
+        {
+            printAttribute(attribute);
+            printAttributes(std::forward<AttributeList>(attributes)...);
+        }
         
     private:
         std::ostream& stream_;
